ad9d82018b28924377ba0623e6f95a2f20b67b1f
Hephzibah Ranjan <hephzibahranjan@gmail.com>
2026-07-23 21:18:21 +0530

    important pattern regarding bitwise xor of adjacent elements

--- r1000/BitManipulation/RoofConstruction.java
+++ r1000/BitManipulation/RoofConstruction.java
@@ -1,5 +1,24 @@
-// package r1000.BitManipulation;
 
+/*
+https://codeforces.com/problemset/problem/1632/B
+
+
+Explanation-
+Important mathematcal observation
+For suppose n=4,
+For any combination of permutation, the maximum xor will always be >=2^k, where k in the highest vhecked bit
+lets take this combination= 0,1,2,3=max(1,3,1)=3
+1,0,2,3=max(1,2,1)
+2,0,1,3=max(2,1,2)
+
+so 0=00, 1=01, 2=10, 3=11, here the highest checked bit is 1, and so 2^1=2, so the max xor will never be less than 2,
+but we have to make sure such that the xor does not get greater than 2^k,
+group 1=where the highest bit here bit 1 is bot checked (0,1)
+group 2=where the highest bit that is bit 1 is checked (2,3)
+so you can rearrange group 1 and 2 anyhow but, where pairing group 1 and group 2,
+always keep 0,2^k, here 0,2 together such that the max xor is 2
+*/
+package r1000.BitManipulation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
